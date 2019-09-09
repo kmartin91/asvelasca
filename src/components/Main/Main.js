edca74cc3ce381cb325861eb100dbcf264d8ba0a
David Gribouille <kevin@martin.best>
2019-09-09 17:42:37 +0200

    prettier

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,9 +5,9 @@ import React from 'react';
 import './Main.common.scss';
 
 const Main = () => (
-    <div className="Main">
-        <div className="Main__title">Hello David!</div>
-    </div>
+  <div className="Main">
+    <div className="Main__title">Hello David!</div>
+  </div>
 );
 
 export default Main;
