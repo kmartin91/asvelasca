3c77c79fa5c3022ffad48ca397e537ba7b5a13e5
David Gribouille <kevin@martin.best>
2019-09-09 17:40:17 +0200

    changing snapshot

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,18 +1,13 @@
 /* @flow */
 
 import React from 'react';
-import { Helmet } from 'react-helmet';
 
 import './Main.common.scss';
 
 const Main = () => (
-  <div className="Main">
-    <Helmet>
-      <title>Coucou David Gribouille</title>
-      <meta charSet="utf-8" />
-    </Helmet>
-    <div className="Main__title">Hello David!</div>
-  </div>
+    <div className="Main">
+        <div className="Main__title">Hello David!</div>
+    </div>
 );
 
 export default Main;
