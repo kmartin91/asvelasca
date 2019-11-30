26b5ef25c02d70f69aa432c0a8b380b69596f7cb
kmartin91 <coldfuzion@ckoisa.net>
2019-11-30 13:02:13 +0100

    third commit

--- index.js
+++ index.js
@@ -26,54 +26,150 @@ ReactDOM.render(
     <Layout path={`${base}/home`} component={Main} withoutFooter />
 
     {/* Progeto */}
-    <Layout path={`${base}/progetto`} withoutFooter component={Edito} page="progetto" />
-    <Layout path={`${base}/project`} withoutFooter component={Edito} page="progetto" />
-    <Layout path={`${base}/projet`} withoutFooter component={Edito} page="progetto" />
+    <Layout
+      path={`${base}/progetto`}
+      withoutFooter
+      component={Edito}
+      page="progetto"
+      name="progetto"
+    />
+    <Layout
+      path={`${base}/project`}
+      withoutFooter
+      component={Edito}
+      page="progetto"
+      name="project"
+    />
+    <Layout path={`${base}/projet`} withoutFooter component={Edito} page="progetto" name="projet" />
 
     {/* Squadra */}
-    <Layout path={`${base}/squadra`} withoutFooter component={Edito} page="squadra" />
-    <Layout path={`${base}/equipe`} withoutFooter component={Edito} page="squadra" />
-    <Layout path={`${base}/team`} withoutFooter component={Edito} page="squadra" />
+    <Layout
+      path={`${base}/squadra`}
+      withoutFooter
+      component={Edito}
+      page="squadra"
+      name="squadra"
+    />
+    <Layout path={`${base}/equipe`} withoutFooter component={Edito} page="squadra" name="equipe" />
+    <Layout path={`${base}/team`} withoutFooter component={Edito} page="squadra" name="team" />
 
     {/* Sponsor */}
-    <Layout path={`${base}/sponsor`} withoutFooter component={Edito} page="sponsor" />
+    <Layout
+      path={`${base}/sponsor`}
+      withoutFooter
+      component={Edito}
+      page="sponsor"
+      name="sponsor"
+    />
 
     {/* Artists */}
-    <Layout path={`${base}/artists`} withoutFooter component={Edito} page="artists" />
-    <Layout path={`${base}/artistes`} withoutFooter component={Edito} page="artists" />
-    <Layout path={`${base}/artisti`} withoutFooter component={Edito} page="artists" />
+    <Layout
+      path={`${base}/artists`}
+      withoutFooter
+      component={Edito}
+      page="artists"
+      name="artists"
+    />
+    <Layout
+      path={`${base}/artistes`}
+      withoutFooter
+      component={Edito}
+      page="artists"
+      name="artistes"
+    />
+    <Layout
+      path={`${base}/artisti`}
+      withoutFooter
+      component={Edito}
+      page="artists"
+      name="artisti"
+    />
 
     {/* Season */}
-    <Layout path={`${base}/season`} withoutFooter component={Edito} page="season" />
-    <Layout path={`${base}/saison`} withoutFooter component={Edito} page="season" />
-    <Layout path={`${base}/stagione`} withoutFooter component={Edito} page="season" />
+    <Layout path={`${base}/season`} withoutFooter component={Edito} page="season" name="season" />
+    <Layout path={`${base}/saison`} withoutFooter component={Edito} page="season" name="saison" />
+    <Layout
+      path={`${base}/stagione`}
+      withoutFooter
+      component={Edito}
+      page="season"
+      name="stagione"
+    />
 
     {/* Tickets */}
-    <Layout path={`${base}/tickets`} withoutFooter component={Edito} page="tickets" />
+    <Layout
+      path={`${base}/tickets`}
+      withoutFooter
+      component={Edito}
+      page="tickets"
+      name="tickets"
+    />
 
     {/* Club */}
-    <Layout path={`${base}/club`} withoutFooter component={Edito} page="club" />
-    <Layout path={`${base}/societa`} withoutFooter component={Edito} page="club" />
+    <Layout path={`${base}/club`} withoutFooter component={Edito} page="club" name="club" />
+    <Layout path={`${base}/societa`} withoutFooter component={Edito} page="club" name="societa" />
 
     {/* Supporters */}
-    <Layout path={`${base}/supporters`} withoutFooter component={Edito} page="supporters" />
-    <Layout path={`${base}/supporteurs`} withoutFooter component={Edito} page="supporters" />
-    <Layout path={`${base}/tifosi`} withoutFooter component={Edito} page="supporters" />
+    <Layout
+      path={`${base}/supporters`}
+      withoutFooter
+      component={Edito}
+      page="supporters"
+      name="supporters"
+    />
+    <Layout
+      path={`${base}/supporteurs`}
+      withoutFooter
+      component={Edito}
+      page="supporters"
+      name="supporteurs"
+    />
+    <Layout
+      path={`${base}/tifosi`}
+      withoutFooter
+      component={Edito}
+      page="supporters"
+      name="tifosi"
+    />
 
     {/* Press */}
-    <Layout path={`${base}/press`} withoutFooter component={Edito} page="press" />
-    <Layout path={`${base}/presse`} withoutFooter component={Edito} page="press" />
-    <Layout path={`${base}/stampa`} withoutFooter component={Edito} page="press" />
+    <Layout path={`${base}/press`} withoutFooter component={Edito} page="press" name="press" />
+    <Layout path={`${base}/presse`} withoutFooter component={Edito} page="press" name="presse" />
+    <Layout path={`${base}/stampa`} withoutFooter component={Edito} page="press" name="stampa" />
 
     {/* Shop */}
-    <Layout path={`${base}/shop`} withoutFooter component={Edito} page="shop" />
+    <Layout path={`${base}/shop`} withoutFooter component={Edito} page="shop" name="shop" />
     {/* Bulletin */}
-    <Layout path={`${base}/bollettino`} withoutFooter component={Edito} page="bulletin" />
-    <Layout path={`${base}/bulletin`} withoutFooter component={Edito} page="bulletin" />
+    <Layout
+      path={`${base}/bollettino`}
+      withoutFooter
+      component={Edito}
+      page="bulletin"
+      name="bollettino"
+    />
+    <Layout
+      path={`${base}/bulletin`}
+      withoutFooter
+      component={Edito}
+      page="bulletin"
+      name="bulletin"
+    />
 
     {/* Contacts */}
-    <Layout path={`${base}/contatti`} withoutFooter component={Edito} page="contacts" />
-    <Layout path={`${base}/contacts`} withoutFooter component={Edito} page="contacts" />
+    <Layout
+      path={`${base}/contatti`}
+      withoutFooter
+      component={Edito}
+      page="contacts"
+      name="contatti"
+    />
+    <Layout
+      path={`${base}/contacts`}
+      withoutFooter
+      component={Edito}
+      page="contacts"
+      name="contacts"
+    />
     {/*}
     <Route
       component={Errors}
